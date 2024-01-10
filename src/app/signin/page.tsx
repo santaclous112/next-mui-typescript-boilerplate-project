4b6f1e8f9b7f35141630305e53e5dbc2c9b3e476
santaclous112 <santaclous112@gmail.com>
2024-01-10 14:07:15 -0800

    update/homepage

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -13,10 +13,7 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-// import { createTheme } from '@mui/material';
-// import { ThemeProvider } from '@emotion/react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { collapseClasses } from '@mui/material';
 
 function Copyright(props: any) {
   return (
